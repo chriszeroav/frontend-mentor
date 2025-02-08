ec1862a7966fc9fe211508b1c60a56d5b2c2871f
Christopher Romero <chris.zeroav@gmail.com>
2025-02-07 20:43:43 -0500

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/OtVaLsBVZ1

---
+++ apps/tech-book-club-landing/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'tech-book-club-landing',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/tech-book-club-landing',
+};
